21371338862c9b5472c57e51668f94423d4b9f08
jyzxc <867137498@qq.com>
2024-11-28 19:13:37 +0800

    feat: add the home page

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,10 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: '/',
+            component: () => import('./views/layout/index.vue'),
+        },
+        {
             path: '/login',
             name: 'login',
             // lazy loading
